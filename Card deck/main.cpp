5e3f2c406ec9bdc69657c622a58a97a9cc2290b1
_Jacce_ <jacob.ogren2@gmail.com>
2020-11-18 16:32:58 +0100

    Added a main function that creates a card deck and then prints all the cards inside the card deck

--- Card deck/main.cpp
+++ Card deck/main.cpp
@@ -1,3 +1,12 @@
+#include "CardDeck.h"
+#include <iostream>
+
 int main() {
+    CardDeck cardDeck;
+    for (int i = 0; i < 55; i++) {
+        cardDeck.cards[i].Print();
+        std::cout << std::endl;
+    }
+    std::cin.get();
     return 0;
 }
