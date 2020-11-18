0602b773f8b9081e6f741b91a14e783a7d5622b0
_Jacce_ <jacob.ogren2@gmail.com>
2020-11-18 20:50:28 +0100

    Use constexpr

--- Card deck/CardDeck.h
+++ Card deck/CardDeck.h
@@ -4,8 +4,9 @@ class CardDeck {
 public:
     CardDeck();
 public:
-    static const int NUM_SUITES = 4;
-    static const int NUM_RANKS  = 13;
-    static const int NUM_JOKERS = 3;
+    static constexpr int NUM_SUITES = 4;
+    static constexpr int NUM_RANKS  = 13;
+    static constexpr int NUM_JOKERS = 3;
+
     Card cards[NUM_SUITES * NUM_RANKS + NUM_JOKERS]{};
 };
