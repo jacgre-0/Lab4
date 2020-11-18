24a4ab4c055d776c3c8455d8872593032d2c54d5
_Jacce_ <jacob.ogren2@gmail.com>
2020-11-18 11:06:41 +0100

    Added Polyline class

---
+++ Polyline/Polyline.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Point2D.h"
+
+class PolyLine {
+public:
+    PolyLine(int length);
+public:
+    void add_point(Point2D point);
+    PolyLine at(int index);
+    void remove_point();
+    bool contains(Point2D point);
+    int get_total_points() const;
+    float total_length();
+    void print_coords() const;
+private:
+    int length;
+    int numPoints{};
+    Point2D* points{};
+    bool isFull{};
+};
