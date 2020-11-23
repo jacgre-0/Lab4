de8e2a976e7cbfdbd69e4dec7d5b546ff9a0e3df
_Jacce_ <jacob.ogren2@gmail.com>
2020-11-23 19:08:07 +0100

    Improved the CardUtils sort function

--- Card utilities/CardUtilities.cpp
+++ Card utilities/CardUtilities.cpp
@@ -9,54 +9,17 @@ void CardUtilities::shuffle(CardDeck* input) {
 }
 
 void CardUtilities::sort(CardDeck* input) {
-    Card clubs[13];
-    Card diamonds[13];
-    Card hearts[13];
-    Card spades[13];
-
-    //First sort the suits, from lowest to highest i.e clubs->spades 
+    //If the lhs and rhs is of the same suit, sort by rank, otherwise
+    //sort by suit
     std::sort(std::begin(input->cards), std::end(input->cards),
         [](const Card& lhs, const Card& rhs)
         {
+            if (lhs.GetSuit().value == rhs.GetSuit().value) {
+                return lhs.GetRank().value < rhs.GetRank().value;
+            }
             return lhs.GetSuit().value < rhs.GetSuit().value;
         }
     );
-
-    //Now we sort each suits rank by splitting each group of suit into chunks of it ranks
-    //We know the 3 last cards will be Joker given the first sort we did, so we don't have to sort the last
-    //3 cards again.
-    for (int i = 0; i < 13; i++) {
-        clubs[i] = input->cards[i];
-    }
-    for (int i = 0; i < 13; i++) {
-        diamonds[i] = input->cards[i+13];
-    }
-    for (int i = 0; i < 13; i++) {
-        hearts[i] = input->cards[i+26];
-    }
-    for (int i = 0; i < 13; i++) {
-        spades[i] = input->cards[i+39];
-    }
-
-    //sort each chunk
-    std::sort(std::begin(clubs),    std::end(clubs));
-    std::sort(std::begin(diamonds), std::end(diamonds));
-    std::sort(std::begin(hearts),   std::end(hearts));
-    std::sort(std::begin(spades),   std::end(spades));
-
-    //put back the sorted chunks in the original array again
-    for (int i = 0; i < 13; i++) {
-        input->cards[i] = clubs[i];
-    }
-    for (int i = 0; i < 13; i++) {
-        input->cards[i+13] = diamonds[i];
-    }
-    for (int i = 0; i < 13; i++) {
-        input->cards[i + 26] = hearts[i];
-    }
-    for (int i = 0; i < 13; i++) {
-        input->cards[i + 39] = spades[i];
-    }
 }
 
 Card* CardUtilities::checkMissing(const CardDeck* input) {
